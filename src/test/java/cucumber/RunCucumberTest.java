e9f961107b38dd2b9b727f3af788a588b052b97d
pi <chm1980@gmail.com>
2025-05-04 10:35:02 -0300

    Commit v-x4

---
+++ src/test/java/cucumber/RunCucumberTest.java
@@ -0,0 +1,11 @@
+package com.yourcompany.cucumber;
+
+import io.cucumber.junit.platform.engine.Cucumber;
+import org.junit.platform.suite.api.Suite;
+import org.junit.platform.suite.api.SelectClasspathResource;
+
+@Cucumber
+@Suite
+@SelectClasspathResource("features") // Certifique-se de que a pasta 'features' esteja em src/test/resources
+public class RunCucumberTest {
+}
