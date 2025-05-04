7be81d8db831fb7783d1fd08740ecc00a5248bfe
pi <chm1980@gmail.com>
2025-05-04 20:01:45 -0300

    Commit v-x23

--- src/test/java/cucumber/RunCucumberTest.java
+++ src/test/java/cucumber/RunCucumberTest.java
@@ -1,11 +1,11 @@
 package cucumber;
 
 import io.cucumber.junit.platform.engine.Cucumber;
-import org.junit.platform.suite.api.Suite;
 import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
 
 @Cucumber
 @Suite
-@SelectClasspathResource("classpath:resources/features") // Certifique-se de que a pasta 'features' esteja em src/test/resources
+@SelectClasspathResource("features")
 public class RunCucumberTest {
 }
