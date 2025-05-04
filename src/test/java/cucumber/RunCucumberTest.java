357326ab261a0640b67ab9649dfb433168085b22
pi <chm1980@gmail.com>
2025-05-04 16:56:21 -0300

    Commit v-x13

--- src/test/java/cucumber/RunCucumberTest.java
+++ src/test/java/cucumber/RunCucumberTest.java
@@ -1,20 +1,4 @@
-<<<<<<< HEAD
-import org.junit.platform.suite.api.ConfigurationParameter;
-import org.junit.platform.suite.api.IncludeEngines;
-import org.junit.platform.suite.api.SelectClasspathResource;
-import org.junit.platform.suite.api.Suite;
-
-import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
-
-@Suite
-@IncludeEngines("cucumber")
-@SelectClasspathResource("Features")
-@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "Steps")
-public class RunCucumberTest {
-}
-
-=======
-package com.yourcompany.cucumber;
+package cucumber;
 
 import io.cucumber.junit.platform.engine.Cucumber;
 import org.junit.platform.suite.api.Suite;
@@ -25,4 +9,3 @@ import org.junit.platform.suite.api.SelectClasspathResource;
 @SelectClasspathResource("features") // Certifique-se de que a pasta 'features' esteja em src/test/resources
 public class RunCucumberTest {
 }
->>>>>>> e9f961107b38dd2b9b727f3af788a588b052b97d
