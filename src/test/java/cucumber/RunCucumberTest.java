9795901a13b18346617c60926fb2efa8c35ca111
pi <chm1980@gmail.com>
2025-05-05 11:29:17 -0300

    Commit v-x28

--- src/test/java/cucumber/RunCucumberTest.java
+++ src/test/java/cucumber/RunCucumberTest.java
@@ -6,6 +6,7 @@ import org.junit.platform.suite.api.Suite;
 
 @Cucumber
 @Suite
+@Cucumber.Options(glue = {"Steps"}) // Use @Cucumber.Options e especifique o pacote em um array de Strings
 @SelectClasspathResource("features")
 public class RunCucumberTest {
 }
